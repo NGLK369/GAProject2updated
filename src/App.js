035703a9a1ac5eaf0aa4eed55b7db63cf141560d
LK MacBook Pro <nlk@MacBookPro.local>
2023-08-12 00:29:08 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom'; // Import Link here
 import './App.css';
+import CurrencyConverter from './Components/CurrencyConverter';
+import About from './Components/About'; // Create this component
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+
+          </ul>
+        </nav>
+        <div className="container">
+          <Routes>
+            <Route path="/" exact element={<CurrencyConverter />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </div>
   );
-}
+};
 
 export default App;
